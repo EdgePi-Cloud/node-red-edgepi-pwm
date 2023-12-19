d8079761c181ebbf30280fbedc7d55ef26258f48
Josiah Kievit <josiahtesfu@gmail.com>
2023-12-19 11:46:43 -0800

    Finish PWM implementation.

--- edgepi-pwm.js
+++ edgepi-pwm.js
@@ -1,53 +1,114 @@
-module.exports = function(RED) {
-    const rpc = require("@edgepi-cloud/edgepi-rpc")
+module.exports = function (RED) {
+  const rpc = require("@edgepi-cloud/edgepi-rpc");
 
-    function PWMNode(config) {
-        // Create node with user config
-        RED.nodes.createNode(this, config);
-        const node = this;
-        const pwmPin = (config.pwmPin);
-        const state = (config.state);
-        const polarity = (config.polarity);
-        const frequency = parseFloat(config.frequency);
-        const dutyCycle = parseFloat(config.dutyCycle);
+  function PWMNode(config) {
+    RED.nodes.createNode(this, config);
+    const node = this;
+    const pwmPin = rpc.PWMPins[config.pwmPin];
 
-        if (pwm){
-            console.debug("PWM node initialized on:", transport);
-            node.status({fill:"green", shape:"ring", text:"PWM initialized"});
+    setInitialConfigs(config)
+      .then((pwm) => {
+        node.on("input", async function (msg, send, done) {
+          node.status({ fill: "green", shape: "dot", text: "input received" });
+          try {
+            await updateConfigsOnInput(msg, pwm, config);
+            msg = {
+              payload: await pwm.getDutyCycle(pwmPin),
+              pwmPin: pwmPin,
+              frequency: await pwm.getFrequency(pwmPin),
+              polarity: await pwm.getPolarity(pwmPin),
+              enabled: await pwm.getEnabled(pwmPin),
+            };
+          } catch (error) {
+            console.error(error);
+            msg.payload = error;
+          }
+          send(msg);
+          if (done) done();
+        });
+
+        node.on("close", async () => {
+          try {
+            await pwm.close(pwmPin);
+          } catch (error) {
+            console.error(error);
+          }
+        });
+      })
+      .catch((error) => {
+        console.error(error);
+        node.status({
+          fill: "red",
+          shape: "ring",
+          text: "Initialization error",
+        });
+      });
+
+    async function setInitialConfigs(config) {
+      const transport =
+        config.transport === "Network"
+          ? `tcp://${config.tcpAddress}:${config.tcpPort}`
+          : "ipc:///tmp/edgepi.pipe";
+      const configArg = {
+        pwmNum: config.pwmPin,
+        frequency: parseFloat(config.frequency),
+        dutyCycle: parseFloat(config.dutyCycle) / 100,
+        polarity: config.polarity,
+      };
+
+      try {
+        const pwm = new rpc.PWMService(transport);
+        console.info("PWM node initialized on:", transport);
+        await pwm.initPwm(pwmPin);
+        await pwm.setConfig(configArg);
+        if (config.state === "enabled") {
+          await pwm.enable(pwmPin);
+        } else {
+          await pwm.disable(pwmPin);
         }
-
-        initializePWM(config).then(pwm => {
-            node.on('input', async function (msg, send, done) {
-                node.status({fill:"green", shape:"dot", text:"input recieved"});
-                try{
-                    // TODO: setting the input configurations... do this later when completing the pr for proper input
-                    const { operation, pin, config } = msg.payload;
-                    // other code for setting inputs. Switch between operations...
-                    // ...
-                    msg.payload = { status: 'Success', operation };
-                }
-                catch(err) {
-                    console.error(err);
-                    msg.payload = err;
-                }
-                // Send message payload
-                send(msg);
-                if (done) {
-                    done();
-                }
-            })
-
-            // handle exit
-            node.on("close", function(done) {
-                node.status({fill:"grey", shape:"ring", text:"PWM terminated"});
-
-                done();
-            });
-        }).catch(error) {
-            // TODO: handle initialization error
-            node.status({fill:"red", shape:"ring", text:"Initialization error"});
-        }
+        return pwm;
+      } catch (error) {
+        error.message =
+          "Error setting initial configurations: " + error.message;
+        throw error;
+      }
     }
-    RED.nodes.registerType("pwm", PWMNode);
 
-}
+    async function updateConfigsOnInput(msg, pwm) {
+      let inputConfig = { pwmNum: pwmPin };
+      const { frequency, payload, polarity, enabled } = msg;
+
+      if (enabled === false) {
+        await pwm.disable(pwmPin);
+      } else {
+        if (payload) {
+          inputConfig.dutyCycle = parseFloat(payload) / 100;
+        }
+        if (frequency) {
+          inputConfig.frequency = parseFloat(frequency);
+        }
+        if (polarity) {
+          inputConfig.polarity = rpc.Polarity[polarity];
+        }
+        // If setConfig fails, reinitialize and try again
+        const maxRetries = 3;
+        let attempt = 0;
+        while (attempt < maxRetries) {
+          try {
+            await pwm.setConfig(inputConfig);
+            break;
+          } catch (error) {
+            attempt++;
+            if (attempt >= maxRetries) throw error;
+            await pwm.initPwm(pwmPin);
+          }
+        }
+        if (enabled === true) {
+          await pwm.enable(pwmPin);
+        }
+      }
+    }
+  }
+
+  RED.nodes.registerType("pwm", PWMNode);
+};
